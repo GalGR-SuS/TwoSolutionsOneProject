9a9fdee80994dd68c1f6ff94ed52785e58927d61
Greenberg, Gal <Gal.Greenberg@surgicalscience.com>
2023-08-13 10:59:54 +0300

    Test case for two solutions overriding a shared build of a shared project

---
+++ SharedProject/Hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void Hello(const char* str)
+{
+    printf("[using SharedProject]\n");
+    printf("I am %s\n", str);
+}
---
+++ Solution2/Project2/Project2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../../SharedProject/Hello.h"
+
+int main()
+{
+	printf("Hello World!\n");
+	Hello("Project 2");
+
+	return 0;
+}
